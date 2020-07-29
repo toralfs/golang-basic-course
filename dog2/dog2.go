1295c7ba06f3b9300eda84fc97e9781ce166a746
toralfs <toralfs97@gmail.com>
2020-07-29 19:00:18 +0200

    renamed to dog2

--- dog2/dog2.go
+++ dog2/dog2.go
@@ -1,5 +1,5 @@
-// Package dog provides functionality related to dogs.
-package dog
+// Package dog2 provides functionality related to dogs.
+package dog2
 
 // Years takes an int of human years and turns it into dog years (1 human year = 7 dog years).
 func Years(hy int) int {

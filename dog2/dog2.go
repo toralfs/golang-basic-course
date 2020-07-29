358cb1b0616ca7cbf2e76b646f943df12ee08905
toralfs <toralfs97@gmail.com>
2020-07-29 19:02:46 +0200

    deleted temporary dog2

--- dog2/dog2.go
+++
@@ -1,7 +0,0 @@
-// Package dog2 provides functionality related to dogs.
-package dog2
-
-// Years takes an int of human years and turns it into dog years (1 human year = 7 dog years).
-func Years(hy int) int {
-	return hy * 7
-}
